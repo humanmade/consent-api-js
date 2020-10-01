cbdda73f0b5a4b883a9cf400db4ebc3c7a14cbc0
Chris Reynolds <chris@jazzsequence.com>
2020-10-01 14:21:02 -0600

    port over the functions to load the js

---
+++ inc/namespace.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Bootstraps the Javascript Consent API.
+ *
+ * @package altis/consent
+ */
+
+namespace Altis\Consent;
+
+/**
+ * Get the plugin data from the file header.
+ */
+function get_plugin_data() {
+	return get_file_data( __FILE__, [ 'Version' => 'Version' ], false );
+}
+
+/**
+ * Enqueue and localize the javascript.
+ */
+function enqueue_api() {
+	$min     = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	$options = get_option( 'cookie_consent_options', [] );
+
+	wp_enqueue_script( 'altis-consent-api', WP_CONSENT_API_URL . "src/wp-consent-api$min.js", [], WP_CONSENT_API_VERSION, true );
+
+	/*
+	 * When the consenttype (optin or optout) can be set dynamically, we can tell plugins to wait in the javascript until the consenttype has been determined.
+	 */
+	$waitfor_consent_hook = apply_filters( 'wp_consent_api_waitfor_consent_hook', false );
+
+	$expiration   = $options['cookie_expiration'] ?: 30;
+	$prefix       = apply_filters( 'wp_consent_cookie_prefix', 'wp_consent' );
+	$consent_type = apply_filters( 'wp_get_consent_type', 'optin' );
+
+	wp_localize_script(
+		'altis-consent-api',
+		'consent_api',
+		[
+			'consent_type'         => $consent_type,
+			'waitfor_consent_hook' => $waitfor_consent_hook,
+			'cookie_expiration'    => $expiration,
+			'cookie_prefix'        => $prefix,
+		]
+	);
+}
